cb647d91dcf4b9596b7edc053e2fced59324c2cf
Angela Truong <angelatru@LAPTOP-QIQU9LHM>
2023-11-05 14:34:57 +1100

    inital commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Create variables to store data in
+// Initialize count as 0
+// listen for clicks on increment button
+// change the count-el in the html to reflect the nre count
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+
+
+
+
+
+
