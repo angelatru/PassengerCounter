5446c8919f6bf984ff756029dcf21df4801eda09
Angela Truong <angelatru@LAPTOP-QIQU9LHM>
2023-11-05 15:22:05 +1100

    Added reset function when pressing save

--- index.js
+++ index.js
@@ -4,14 +4,21 @@
 // change the count-el in the html to reflect the nre count
 
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
-
+// Save function, logs out count when called (ie when clicked)
+function save() { 
+    let saveCount = count + " - "
+    saveEl.textContent += saveCount
+    countEl.textContent = 0
+    count = 0
+}
 
 
 
